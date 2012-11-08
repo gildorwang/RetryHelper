4d31110300ab9133d70609236929e627b6e54224
Gildor <gildorwang@gmail.com>
2012-11-08 11:38:25 +0800

    Intial extraction of RetryHelper
    Should be a working copy.

---
+++ RetryHelper/Extensions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace RetryHelper
+{
+    public static class Extensions
+    {
+        #region TraceSource
+
+        /// <summary>
+        /// Writes an error message to the trace listeners in the System.Diagnostics.TraceSource.Listeners
+        /// collection using the specified object array and formatting information.
+        /// </summary>
+        /// <param name="traceSource">The trace source instance.</param>
+        /// <param name="message">A composite format string that contains text intermixed with zero or more
+        /// format items, which correspond to objects in the args array.</param>
+        public static void TraceError(this TraceSource traceSource, string message)
+        {
+            traceSource.TraceData(TraceEventType.Error, 0, message);
+        }
+
+        /// <summary>
+        /// Writes a verbose message to the trace listeners in the System.Diagnostics.TraceSource.Listeners
+        /// collection using the specified object array and formatting information.
+        /// </summary>
+        /// <param name="traceSource">The trace source instance.</param>
+        /// <param name="message">A composite format string that contains text intermixed with zero or more
+        /// format items, which correspond to objects in the args array.</param>
+        public static void TraceVerbose(this TraceSource traceSource, string message)
+        {
+            traceSource.TraceData(TraceEventType.Verbose, 0, message);
+        }
+
+        /// <summary>
+        /// Writes an error message to the trace listeners in the System.Diagnostics.TraceSource.Listeners
+        /// collection using the specified object array and formatting information.
+        /// </summary>
+        /// <param name="traceSource">The trace source instance.</param>
+        /// <param name="format">A composite format string that contains text intermixed with zero or more
+        /// format items, which correspond to objects in the args array.</param>
+        /// <param name="args">An array containing zero or more objects to format.</param>
+        public static void TraceError(this TraceSource traceSource, string format, params object[] args)
+        {
+            traceSource.TraceError(string.Format(CultureInfo.InvariantCulture, format, args));
+        }
+
+        /// <summary>
+        /// Writes a verbose message to the trace listeners in the System.Diagnostics.TraceSource.Listeners
+        /// collection using the specified object array and formatting information.
+        /// </summary>
+        /// <param name="traceSource">The trace source instance.</param>
+        /// <param name="format">A composite format string that contains text intermixed with zero or more
+        /// format items, which correspond to objects in the args array.</param>
+        /// <param name="args">An array containing zero or more objects to format.</param>
+        public static void TraceVerbose(this TraceSource traceSource, string format, params object[] args)
+        {
+            traceSource.TraceVerbose(string.Format(CultureInfo.InvariantCulture, format, args));
+        }
+
+        #endregion
+
+       #region Delegates
+
+        /// <summary>
+        /// Makes an <see cref="Func{TResult}"/> instance from an <see cref="Action"/> instance. 
+        /// The <see cref="Func{TResult}"/> instance would execute the <see cref="Action"/> delegate
+        /// and return the specified value when being called.
+        /// </summary>
+        /// <typeparam name="T">The return type of <see cref="Func{TResult}"/>.</typeparam>
+        /// <param name="action">The <see cref="Action"/> instance.</param>
+        /// <param name="value">The return value of the <see cref="Func{TResult}"/> instance.</param>
+        /// <returns></returns>
+        public static Func<T> MakeFunc<T>(this Action action, T value = default(T))
+        {
+            if (action == null)
+                throw new ArgumentNullException("action");
+
+            return () =>
+            {
+                action();
+                return value;
+            };
+        }
+
+        #endregion
+    }
+}
---
+++ RetryHelper/RetryHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RetryHelper
+{
+    /// <summary>
+    /// Help retry a delegate until a given condition is met.
+    /// </summary>
+    public class RetryHelper
+    {
+        public static readonly string DefaultTraceSourceName = "retryHelperTrace";
+        
+        protected readonly TraceSource TraceSource;
+
+        #region Singleton
+
+        private static readonly Lazy<RetryHelper> _instance =
+            new Lazy<RetryHelper>(() => new RetryHelper());
+
+        /// <summary>
+        /// Get the default <see cref="RetryHelper"/> instance which uses the default trace source name.
+        /// </summary>
+        public static RetryHelper Instance
+        {
+            get { return _instance.Value; }
+        }
+
+        #endregion
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "RetryHelper" /> class with the default trace source name.
+        /// </summary>
+        public RetryHelper() : this(new TraceSource(DefaultTraceSourceName))
+        {
+
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "RetryHelper" /> class.
+        /// </summary>
+        /// <param name = "traceSource">The trace source.</param>
+        public RetryHelper(TraceSource traceSource)
+        {
+            TraceSource = traceSource;
+            DefaultMaxTryCount = RetryTask.DefaultMaxTryCount;
+            DefaultTryInterval = RetryTask.DefaultTryInterval;
+            DefaultMaxTryTime = RetryTask.DefaultMaxTryTime;
+        }
+
+        /// <summary>
+        /// Gets or sets the default max try time.
+        /// </summary>
+        /// <value>
+        /// The default max try time.
+        /// </value>
+        public TimeSpan DefaultMaxTryTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default try interval.
+        /// </summary>
+        /// <value>
+        /// The default try interval.
+        /// </value>
+        public TimeSpan DefaultTryInterval { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default max try count.
+        /// </summary>
+        /// <value>
+        /// The default max try count.
+        /// </value>
+        public int DefaultMaxTryCount { get; set; }
+
+        /// <summary>
+        /// Builds a retry task from the specified delegate.
+        /// </summary>
+        /// <typeparam name="T">Type of return value of the delegate.</typeparam>
+        /// <param name="func">The delegate to try.</param>
+        /// <returns></returns>
+        public RetryTask<T> Try<T>(Func<T> func)
+        {
+            return new RetryTask<T>(func, TraceSource, DefaultMaxTryTime, DefaultMaxTryCount, DefaultTryInterval);
+        }
+
+        /// <summary>
+        /// Builds a retry task from the specified delegate.
+        /// </summary>
+        /// <param name="action">The delegate to try.</param>
+        /// <returns></returns>
+        public RetryTask Try(Action action)
+        {
+            return new RetryTask(action, TraceSource, DefaultMaxTryTime, DefaultMaxTryCount, DefaultTryInterval);
+        }
+    }
+}
